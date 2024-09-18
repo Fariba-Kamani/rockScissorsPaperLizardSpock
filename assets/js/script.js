7e0fea7938f4e1f56e50ca994086408093ec184b
Seyedehfariba Kamani <fariba.kamani87@gmail.com>
2024-09-18 13:17:44 +0000

    Add html and css code for game start section

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+function startGame() {
+
+}
+
+function runGame() {
+
+}
+
+function showUsersChoice() {
+
+}
+
+function showComputersChoice() {
+
+}
+
+function countUsersScore() {
+
+}
+
+function countComputersScore() {
+
+}
+
+function ShowResult() {
+
+}
+
+function gameRules() {
+
+}
+
+function exitGame() {
+
+}
