9ca65bf29b8a9657b8df248efb5e11c5859bef6b
Seyedehfariba Kamani <fariba.kamani87@gmail.com>
2024-09-18 16:58:08 +0000

    Add code to JS to get the user's name from the form text input to use for user's score. Add eventlistener to hide the section with start-game class and show the game section by submitting the form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,26 @@
-function startGame() {
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    let userName = document.getElementById("user-name").value;
+    
+    // Hide elements with class 'start-game'
+    document.querySelectorAll(".start-game").forEach(element => {
+        element.style.display = "none";
+    });
+     // Show elements with class 'result-display-area'
+     document.querySelectorAll(".result-display-area").forEach(element => {
+        element.style.display = "flex";
+    });
+    
+    // Show elements with class 'controls-area'
+    document.querySelectorAll(".controls-area").forEach(element => {
+        element.style.display = "flex";
+    });
+    
+    runGame(userName);
+});
 
-}
-
-function runGame() {
+function runGame(user) {
+    document.getElementById('player').innerHTML = `${user}'s`;
 
 }
 
