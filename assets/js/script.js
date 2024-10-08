e83feb618ed5f809db4f5bc9d8917c77740394ae
Seyedehfariba Kamani <fariba.kamani87@gmail.com>
2024-10-08 12:12:08 +0000

    Modif code in js to only have one eventListener for .rules, one function to handle the rules display and one to close the rules display to solve the bugg caused by opening the rules during the result display phase of the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
-
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     document.getElementById('user-name').focus();
     document.getElementById('user-name').value = "";
 })
@@ -8,10 +7,10 @@ document.querySelector('form').addEventListener('submit', function (event) {
     let userName = document.getElementById("user-name").value;
 
     // Text input validation
-    if(userName.length > 12 || userName === "" || userName.includes(' ')) {
+    if (userName.length > 12 || userName === "" || userName.includes(' ')) {
         document.getElementById('error-message').style.display = 'block';
         // Hide error message after 3 seconds
-        setTimeout(function() {
+        setTimeout(function () {
             document.getElementById('error-message').style.display = 'none';
         }, 3000); // 3000 milliseconds = 3 seconds
         return;
@@ -29,12 +28,12 @@ document.querySelector('form').addEventListener('submit', function (event) {
     document.querySelectorAll(".controls-area").forEach(element => {
         element.style.display = "flex";
     });
-    
+
     setUser(userName);
-    
+
 });
 
-document.querySelector('.rules').addEventListener('click', gameRules1);
+document.querySelector('.rules').addEventListener('click', gameRules);
 
 function setUser(user) {
     document.getElementById('player').innerHTML = `${user}'s`;
@@ -43,7 +42,7 @@ function setUser(user) {
 }
 
 function runGame() {
-   
+
     // Add an event listener to button elements
     let buttons = document.getElementsByTagName('button');
     let playerChoice;
@@ -75,13 +74,11 @@ function runGame() {
                     showUsersChoice(playerChoice);
                     showComputersChoice();
                     break;
-                case 'rules':
-                    gameRules2();
-                    break;
             }
-    });
+        });
 
-}}
+    }
+}
 
 function showUsersChoice(playerChoice) {
     document.getElementById('user-pick').innerHTML = playerChoice;
@@ -98,14 +95,14 @@ function showComputersChoice() {
 function countUsersScore() {
     let userScore = parseInt(document.getElementById('user-counter').innerText);
     document.getElementById('user-counter').innerText = ++userScore;
-    
+
 
 }
 
 function countComputersScore() {
     let computerScore = parseInt(document.getElementById('pc-counter').innerText);
     document.getElementById('pc-counter').innerText = ++computerScore;
-    
+
 
 }
 
@@ -124,8 +121,8 @@ function ShowResult() {
     let player = document.getElementById("user-name").value;
     result.style.display = 'block';
 
-     // Hide the control-area-heading during result display
-     document.querySelectorAll(".controls-area").forEach(element => {
+    // Hide the control-area-heading during result display
+    document.querySelectorAll(".controls-area").forEach(element => {
         element.style.display = "none";
     });
 
@@ -234,18 +231,14 @@ function ShowResult() {
                 break;
 
         }
-        
+
     }
-    roundCounter();  
-    document.querySelectorAll(".rules").forEach(element => {
-        element.style.color = "#e2f5e2";
-    });
-     // Show the result for 3 seconds and then reset
+    roundCounter();
+
+    // Show the result for 3 seconds and then reset
     setTimeout(function () {
-        result.style.display = 'none';  // Hide result after 3 seconds
-        document.querySelectorAll(".rules").forEach(element => {
-            element.style.color = "#00D26A";
-        });
+        result.style.display = 'none'; // Hide result after 3 seconds
+
 
         // Reset user and computer choices to ✊
         document.getElementById('user-pick').innerHTML = '✊';
@@ -255,116 +248,143 @@ function ShowResult() {
         document.querySelectorAll(".controls-area").forEach(element => {
             element.style.display = "flex";
         });
-    }, 3000);  // 3 seconds delay 
+    }, 3000); // 3 seconds delay 
 
 }
 
 function roundCounter() {
     let myCounter = parseInt(document.getElementById('round-count').innerText);
-    
+
     // If the round count reaches 7, show final result and exit game
     if (myCounter === 7) {
         document.querySelectorAll(".control-btn").forEach(element => {
-            element.style.display = "none";});
+            element.style.display = "none";
+        });
         document.getElementById('control-area-heading').innerHTML = "Wait to see the final result ...";
-         // Delay the execution of exitGame() by 6 seconds (3000 milliseconds)
-         setTimeout(function() {
-            exitGame();  // Exit game after a delay
+        // Delay the execution of exitGame() by 6 seconds (3000 milliseconds)
+        setTimeout(function () {
+            exitGame(); // Exit game after a delay
         }, 7000);
     } else if (myCounter < 7) {
         // Increment and update the round count
         myCounter++;
         document.getElementById('round-count').innerText = myCounter;
-    } 
-}
-
-function gameRules1() {
-    document.querySelectorAll(".rules-explain").forEach(element => {
-        element.style.display = "block";
-    });
-    document.querySelectorAll(".start-game").forEach(element => {
-        element.style.display = "none";
-    });
-
-    document.querySelector("#close-rules").addEventListener('click', closeRules1);
-    document.querySelector("#close-rules").addEventListener("keydown", function(event) {
-        // Check if the pressed key is "Enter"
-        if (event.key === "Enter") {
-          closeRules1();
-        }
-      }); 
     }
-
-// Separate function to show the rules, reused in both conditions
-function gameRules2() {
-    document.querySelectorAll(".rules-explain").forEach(element => {
-        element.style.display = "block";
-    });
-    document.querySelectorAll(".result-display-area").forEach(element => {
-        element.style.display = "none";
-    });
-
-    // Hide elements with class 'controls-area'
-    document.querySelectorAll(".controls-area").forEach(element => {
-        element.style.display = "none";
-    });
-
-    // Event listener for closing the rules section
-    document.querySelector("#close-rules").addEventListener('click', closeRules2);
-    document.querySelector("#close-rules").addEventListener("keydown", function(event) {
-        if (event.key === "Enter") {
-            closeRules2();
-        }
-    });
 }
 
-    function closeRules1() {
-        let endGameElements = document.querySelectorAll(".end-game");
-    
-        // Check if all elements are hidden
-        let isVisible = Array.from(endGameElements).every(element => element.style.display === "block");
-        if(isVisible) {
-            document.querySelectorAll(".rules-explain").forEach(element => {
-                element.style.display = "none";
-            });
-    
-        } else {
+function gameRules() {
+    let textResult = document.getElementById('text-result');
+    let rulesExplain = document.querySelectorAll(".rules-explain");
+    let controlsArea = document.querySelectorAll(".controls-area");
+    let resultDisplayArea = document.querySelectorAll(".result-display-area");
+    let startGame = document.querySelectorAll(".start-game");
+    let flag;
+    let player = document.getElementById("player").innerHTML;
+    let rulesElement = document.querySelector(".rules");
+
+    // Text input validation
+
+    //1- for if rules clicked during result display
+    // Check if the result is currently being displayed
+    if (textResult.style.display === 'block') {
+        // Alert the user that the rules can't be shown during result display
+        alert("The rules can't be shown while the result is displayed.");
+        // Remove hover effect by adding a class that disables it
+        rulesElement.classList.add("no-hover");
+        // Reset hover effect
+        rulesElement.classList.remove("no-hover");
+        // Return early to prevent the rules from being shown
+        return;
+    } else if (player === "") {
+        //for if the rules clicked from login
+        flag = 1;
+        rulesExplain.forEach(element => {
+            element.style.display = "block";
+        });
+        startGame.forEach(element => {
+            element.style.display = "none";
+        });
+
+        document.querySelector("#close-rules").addEventListener('click', function () {
+            closeRules(flag);
+        });
+        document.querySelector("#close-rules").addEventListener("keydown", function (event) {
+            // Check if the pressed key is "Enter"
+            if (event.key === "Enter") {
+                closeRules(flag);
+            }
+        });
+
+    } else {
+        flag = 2;
+        //for if rules clicked from game section
+        rulesExplain.forEach(element => {
+            element.style.display = "block";
+        });
+        resultDisplayArea.forEach(element => {
+            element.style.display = "none";
+        });
+
+        // Hide elements with class 'controls-area'
+        controlsArea.forEach(element => {
+            element.style.display = "none";
+        });
+
+        // Event listener for closing the rules section
+        document.querySelector("#close-rules").addEventListener('click', function () {
+            closeRules(flag)
+        });
+        document.querySelector("#close-rules").addEventListener("keydown", function (event) {
+            if (event.key === "Enter") {
+                closeRules(flag);
+            }
+        });
+
+    }
+}
+
+function closeRules(flag) {
+    let endGameElements = document.querySelectorAll(".end-game");
+
+    // Check if all elements are hidden
+    let isVisible = Array.from(endGameElements).every(element => element.style.display === "block");
+    if (isVisible) {
         document.querySelectorAll(".rules-explain").forEach(element => {
+            element.style.display = "none";
+        });
+
+    } else if (flag === 1) {
+        document.querySelectorAll(".rules-explain").forEach(element => {
+            element.style.display = "none";
+        });
+        document.querySelectorAll(".result-display-area").forEach(element => {
+            element.style.display = "none";
+        });
+
+        // Show elements with class 'controls-area'
+        document.querySelectorAll(".controls-area").forEach(element => {
             element.style.display = "none";
         });
         document.querySelectorAll(".start-game").forEach(element => {
             element.style.display = "flex";
         });
-    }
-    
-    }
-
-function closeRules2() {
-    let endGameElements = document.querySelectorAll(".end-game");
-
-    // Check if all elements are hidden
-    let isVisible = Array.from(endGameElements).every(element => element.style.display === "block");
-    if(isVisible) {
+    } else {
         document.querySelectorAll(".rules-explain").forEach(element => {
             element.style.display = "none";
         });
+        document.querySelectorAll(".start-game").forEach(element => {
+            element.style.display = "none";
+        });
+        document.querySelectorAll(".result-display-area").forEach(element => {
+            element.style.display = "flex";
+        });
 
-    } else {
-    document.querySelectorAll(".rules-explain").forEach(element => {
-        element.style.display = "none";
-    });
-    document.querySelectorAll(".start-game").forEach(element => {
-        element.style.display = "none";
-    });
-    document.querySelectorAll(".result-display-area").forEach(element => {
-        element.style.display = "flex";
-    });
+        // Show elements with class 'controls-area'
+        document.querySelectorAll(".controls-area").forEach(element => {
+            element.style.display = "flex";
+        });
 
-    // Show elements with class 'controls-area'
-    document.querySelectorAll(".controls-area").forEach(element => {
-        element.style.display = "flex";
-    });
-}
+    }
 
 }
 
@@ -383,10 +403,10 @@ function exitGame() {
         element.style.display = "block";
     });
 
-    if(userScore === computerScore) {
-       document.getElementById('round-result').innerText = "It's a tie!";
-       document.getElementById('round-result').style.color = 'rgb(3, 3, 14)';
-    } else if(userScore > computerScore) {
+    if (userScore === computerScore) {
+        document.getElementById('round-result').innerText = "It's a tie!";
+        document.getElementById('round-result').style.color = 'rgb(3, 3, 14)';
+    } else if (userScore > computerScore) {
         document.getElementById('round-result').innerText = "You win!";
         document.getElementById('round-result').style.color = '#00D26A';
     } else {
@@ -394,14 +414,14 @@ function exitGame() {
         document.getElementById('round-result').style.color = 'rgb(255, 0, 17)';
     }
     let buttons = document.getElementsByClassName('yes-no-buttons');
-        for (let button of buttons) {
-            button.removeEventListener('click', quitContinue); // Prevent duplicate listeners
-            button.addEventListener('click', quitContinue); // Attach event listener
-        } 
+    for (let button of buttons) {
+        button.removeEventListener('click', quitContinue); // Prevent duplicate listeners
+        button.addEventListener('click', quitContinue); // Attach event listener
+    }
 }
 
 function quitContinue() {
-    document.getElementById('control-area-heading').innerHTML =`Round <span id="round-count">1</span><br>Choose your weapon!`;
+    document.getElementById('control-area-heading').innerHTML = `Round <span id="round-count">1</span><br>Choose your weapon!`;
     document.getElementById('user-counter').innerText = "0";
     document.getElementById('pc-counter').innerText = "0";
     document.getElementById('text-result').innerHTML = "";
@@ -413,21 +433,20 @@ function quitContinue() {
             document.querySelectorAll(".result-display-area").forEach(element => {
                 element.style.display = "flex";
             });
-        
+
             document.querySelectorAll(".controls-area").forEach(element => {
                 element.style.display = "flex";
             });
 
             document.querySelectorAll(".control-btn").forEach(element => {
-                element.style.display = "grid";});
+                element.style.display = "grid";
+            });
 
             document.getElementById('computer-pick').innerHTML = "✊";
             document.getElementById('user-pick').innerHTML = "✊";
             break;
         case 'no':
-           location.reload(true);
+            location.reload(true);
             break;
-        }
-
-
+    }
 }
